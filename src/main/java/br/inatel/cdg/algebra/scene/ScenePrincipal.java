28a79aa766e8eab2a707757903ae836a5e0a6d05
LucasHGraciano <lucasgraciano@gec.inatel.br>
2021-09-09 23:53:09 -0300

    Update ScenePrincipal.java

--- src/main/java/br/inatel/cdg/algebra/scene/ScenePrincipal.java
+++ src/main/java/br/inatel/cdg/algebra/scene/ScenePrincipal.java
@@ -32,14 +32,14 @@ public class ScenePrincipal {
         txtP2Y = new TextField();
 
         //HBox é usado para agrupar elementos horizontalmente
-        HBox hboxP1X = new HBox(lblP1X, txtP1X); //Criamos quatro grupos horizontais com Rótulo e Área de Texto para a coordenada
-        HBox hboxP1Y = new HBox(lblP1Y, txtP1Y);
-        HBox hboxP2X = new HBox(lblP2X, txtP2X);
-        HBox hboxP2Y = new HBox(lblP2Y, txtP2Y);
+        VBox vboxP1X = new VBox(lblP1X, txtP1X); //Criamos quatro grupos verticais com Rótulo e Área de Texto para a coordenada
+        VBox vboxP1Y = new VBox(lblP1Y, txtP1Y);
+        VBox vboxP2X = new VBox(lblP2X, txtP2X);
+        VBox vboxP2Y = new VBox(lblP2Y, txtP2Y);
 
-        //VBox é usada para agrupar elementos verticalmente
+        //HBox é usada para agrupar elementos horizontal
         //No construtor passamos todos os elementos que serão agrupados
-        VBox vboxEntradaCoord = new VBox(hboxP1X,hboxP1Y,hboxP2X,hboxP2Y);//Aqui vamos agrupar verticalmente os pontos para o usuário entrar com as coordenadas dos pontos
+        VBox vboxEntradaCoord = new VBox(vboxP1X,vboxP1Y,vboxP2X,vboxP2Y);//Aqui vamos agrupar verticalmente os pontos para o usuário entrar com as coordenadas dos pontos
 
         //Caixas de texto que apresentaremos o resultado
         txtCoefAngular = new TextField();
@@ -54,28 +54,27 @@ public class ScenePrincipal {
         HBox hboxRespostas = new HBox(txtCoefAngular,txtCoefLinear);
 
         //Criamos o botão
-        btnCalcCoefAngular = new Button("Calcula Coeficiente Angular");
+        btnCalcCoefAngular = new Button("Calcula CA");
         //Criamos a ação que o botão responderá as ser pressionado
         btnCalcCoefAngular.setOnAction(evento -> {
             Reta reta = construirReta();
             txtCoefAngular.setText("Coef Angular: " + reta.calcCoeficienteAngular());//Acessamos o componente textCoefAngular para colocar o resultado do cálculo
         });
 
-        btnCalcCoefLinear = new Button("Calcula Coeficiente Linear");
+        btnCalcCoefLinear = new Button("Calcula CL");
         btnCalcCoefLinear.setOnAction(evento -> {
             Reta reta = construirReta();
             txtCoefLinear.setText("Coef Linear: " + reta.calcCoeficienteLinear());
         });
 
-        //Agrupamos os botões verticalmente
+        //Agrupamos os botões horizontalmente
         HBox hBoxBotoes = new HBox(btnCalcCoefAngular,btnCalcCoefLinear);
 
-
         //Finalmente criamo o layout vertical final!
-        VBox layoutFinal = new VBox(vboxEntradaCoord,hboxRespostas, hBoxBotoes);
+        VBox layoutFinal = new VBox(vboxEntradaCoord,hBoxBotoes,hboxRespostas);
 
         //Criamos a Scene
-        Scene scene = new Scene(layoutFinal, 300 , 200);
+        Scene scene = new Scene(layoutFinal, 500 , 500);
 
         stage.setTitle("Software Para Calculos de Álgebra Linear");
         stage.setScene(scene);
@@ -93,5 +92,4 @@ public class ScenePrincipal {
         Reta reta = new Reta(p1,p2);
         return reta;
     }
-
 }
